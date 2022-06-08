357a4e2986138484478bb8f00a6b03572ae75b41
Tom Newton <tom.newton@schibsted.com>
2022-06-08 08:40:54 +0200

    filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const Data = [
+  { make: "gibson", model: "strat" },
+  { make: "fender", model: "strat" },
+  { make: "axe", model: "strat" },
+];
 
 function App() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const getSelectedCategories = (category: string) => {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(
+        selectedCategories.filter((item) => item !== category)
+      );
+      return;
+    }
+    setSelectedCategories([...selectedCategories, category]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.filter(({ make }) =>
+        selectedCategories.length !== 0
+          ? selectedCategories.includes(make)
+          : selectedCategories
+      ).map((item) => (
+        <Card make={item.make} model={item.model} />
+      ))}
+
+      {Data.map(({ make }, index) => (
+        <div style={{ color: "blue" }}>
+          <label htmlFor={make}>{make}</label>
+          <input
+            key={index}
+            id={make}
+            type="checkbox"
+            value={make}
+            onChange={(e) => getSelectedCategories(e.target.value)}
+          />
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+interface CardProps {
+  make: string;
+  model: string;
+}
+
+const Card = ({ make, model }: CardProps) => {
+  return (
+    <div style={{ background: "lightgrey", padding: "20px", margin: "10px" }}>
+      <h2>{make}</h2>
+      <p>{model}</p>
+    </div>
+  );
+};
